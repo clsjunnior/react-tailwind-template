9261bd99a41226eb3b7270400d70b9abcb2be842
Celso Junior <clsjunnior@live.com>
2022-08-04 22:56:07 -0300

    Update Router.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,7 +4,7 @@ import { Home } from './pages/Home'
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/react-tailwind-template/" element={<Home />} />
     </Routes>
   )
 }
